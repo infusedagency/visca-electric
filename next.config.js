51feb2a43fa0eb44d0b9029ff6de266946bd1eb2
JD Fillmore <jd@infused.agency>
2024-02-03 14:43:42 -0500

    301 redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/services/",
+        destination: "/smart-homes",
+        permanent: true,
+      },
+      {
+        source: "/home-automation/",
+        destination: "/smart-homes",
+        permanent: true,
+      },
+      {
+        source: "/contact/request-a-quote/",
+        destination: "/request-a-quote",
+        permanent: true,
+      },
+      {
+        source: "/contact/request-service-call/",
+        destination: "/request-service-call",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
