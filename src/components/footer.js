47214e673add06926cac6ca01244ec508c7724f1
infusedagency <35544072+infusedagency@users.noreply.github.com>
2024-03-04 09:10:58 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="pt-4 pb-4 lg:pt-8 lg:pb-8 bg-grey">
@@ -5,14 +7,18 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row">
           <div className="flex-1">
             <p className="opacity-100">
-              5602 George St., Unit 4, Niagara Falls, Ontario, L2E 3E2 | <a href="tel:905-354-0184">905-354-0184</a> |{" "}
+              5602 George St., Unit 4, Niagara Falls, Ontario, L2E 3E2 |{" "}
+              <a href="tel:905-354-0184">905-354-0184</a> |{" "}
               <a href="mailto:info@viscaelectric.ca">info@viscaelectric.ca</a>
             </p>
           </div>
           <div className="flex flex-1 justify-normal lg:justify-end">
             <p className="opacity-100">
               {new Date().getFullYear()} 2024 VISCA electric |{" "}
-              <a target="_blank" href="https://www.termsfeed.com/live/f736d78e-6b52-4a55-9166-96e055ab23d6">
+              <a
+                target="_blank"
+                href="https://www.termsfeed.com/live/f736d78e-6b52-4a55-9166-96e055ab23d6"
+              >
                 Privacy Policy
               </a>
             </p>
@@ -21,11 +27,8 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row">
           <div className="flex-1">
             <p className="infused">
-                  Website by Infused Agency | {" "}
-                  <a href="https://infused.agency" target="_blank" title="Web Design St. Catharines">
-                    Niagara Web Design
-                  </a>
-                </p>
+              Website by <Link href="/infused-agency">Infused Agency</Link>
+            </p>
           </div>
         </div>
       </div>
